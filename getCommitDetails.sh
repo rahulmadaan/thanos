fbe3653929d77ae267825aa997995f183e3438fd
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2018-12-19 00:15:14 +0530

    removed comments from code

--- getCommitDetails.sh
+++ getCommitDetails.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
 
-#url=`cat internURL.txt`
-#length=`cat internURL.txt | wc -l`
-#count=1
-#for i in $url; do 
-#  cd internsRepos
-#  userName=`echo $i | cut -d'/' -f5`
-#  percentage=$(echo 'scale=2;'"$count * 100 / $length"|bc)
-#  echo -ne '\0015'
-#  echo -ne `node ../progressBar.js $length $count` $percentage
-#  count=$((count+1))
-#  
-#  if [ ! -d ${userName} ]; then
-#    git clone $i 1>/dev/null 2>/dev/null
-#  else
-#    cd $userName
-#    git pull 1>/dev/null 2>/dev/null
-#    cd ..
-#  fi
-#  cd ..
-#done;
+url=`cat internURL.txt`
+length=`cat internURL.txt | wc -l`
+count=1
+for i in $url; do 
+  cd internsRepos
+  userName=`echo $i | cut -d'/' -f5`
+  percentage=$(echo 'scale=2;'"$count * 100 / $length"|bc)
+  echo -ne '\0015'
+  echo -ne `node ../progressBar.js $length $count` $percentage
+  count=$((count+1))
+
+  if [ ! -d ${userName} ]; then
+    git clone $i 1>/dev/null 2>/dev/null
+  else
+    cd $userName
+    git pull 1>/dev/null 2>/dev/null
+    cd ..
+  fi
+  cd ..
+done;
 userNames=`ls ./internsRepos | grep 'head'`
 rm -rf userData
 mkdir userData
